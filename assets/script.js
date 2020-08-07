9d1024bf0a5d67d5874ad622ef07fb9aea8e2a5d
Ana Luna <luna.ana92@gmail.com>
2020-08-07 00:01:26 -0700

    added an API

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,27 @@
-var apiKey= "15e194eb7337b7a0b68384d6ffba65cc";
+var apiKey = "166a433c57516f51dfab1f7edaed8413";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+    "q=Bujumbura,Burundi&appid=" + apiKey;
+    
+$("#search-Btn").on("click", search);
+let searchCity;
+    function search(e) {
+        e.preventDefault();
+        console.log(this);
+        searchCity = $("#search-city").val();
+    };
+
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(weatherURL);
+    event.preventDefault;
+    searchCity = $("#search-city").val();
+    $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+    $(".wind").text("Wind Speed: " + response.wind.speed);
+    $(".humidity").text("Humidity: " + response.main.humidity);
+});
+
+
+
