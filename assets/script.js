8541714494c829a94595da045ab7cade92d562bb
Ana Luna <luna.ana92@gmail.com>
2020-08-08 00:26:12 -0700

    added weather conditions to the dashboard

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ $("#find-city").on("click", function (event) {
   event.preventDefault();
   var city = $("#search-city").val();
   var apiKey = "15e194eb7337b7a0b68384d6ffba65cc";
-  var queryURL = "api.openweathermap.org/data/2.5/forecast?q={city name}&appid=" + city + apiKey;
+  var queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + encodeURIComponent(city) + "&appid=" + apiKey;
   $.ajax({
     url: queryURL,
     method: "GET"
@@ -11,10 +11,40 @@ $("#find-city").on("click", function (event) {
   });
 });
 
+function displayWeather(){
+  var weatherDiv = $("#results-card");
+
+  var currentDiv = $("<div " + "</div>");
+  var searchedCity = $("<h1>" + "City" + "</h1>");
+  var temperature = $("<p>" + "Temperature:" +"</p>");
+  var humidity = $("<p>" + "Humidity:" + "</p>");
+  var wind = $("<p>" + "Wind" + "</p>");
+  var uvIndex = $("<p>" + "UV Index" + "</p>");
+
+  currentDiv.append(searchedCity);
+  currentDiv.append(temperature);
+  currentDiv.append(humidity);
+  currentDiv.append(wind);
+  currentDiv.append(uvIndex);
+  weatherDiv.append(currentDiv);
+};
+
+displayWeather();
 
 
 
-//Set date for weather forecast
+
+
+
+
+
+
+
+
+
+
+
+// //Set date for weather forecast
 // function setDate() {
 //   var setDate = $("#currentDate");
 //   var date = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");;
