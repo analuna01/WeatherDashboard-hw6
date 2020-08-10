5a59cf93c73c41493f5ee617184bc0db7fa73fba
Ana Luna <luna.ana92@gmail.com>
2020-08-10 01:34:49 -0700

    added ajax methods and date function

--- assets/script.js
+++ assets/script.js
@@ -3,15 +3,42 @@ $("#find-city").on("click", function (event) {
   event.preventDefault();
   var city = $("#search-city").val();
   var apiKey = "15e194eb7337b7a0b68384d6ffba65cc";
-  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + encodeURIComponent(city) + ",Burundi&appid=" + apiKey;
+
+  // AJAX for Weather Info:
   $.ajax({
-    url: queryURL,
+    url: "https://api.openweathermap.org/data/2.5/weather?q=" + encodeURIComponent(city) + ",Burundi&appid=" + apiKey,
     method: "GET"
   }).then(function (response) {
     console.log(response);
     console.log(response.main.temp);
+    $(".city").html("<h1>" + response.name + "</h1>");
+    $(".humidity").text("Humidity:" + response.main.humidity + "%");
+    $(".wind").text("Wind:" + response.wind.speed);
 
-    updateWeather(response.name, response.main.temp, response.main.humidity, response.wind.speed, "");
+  // Converts temp from Kelvin to Fahrenheit:
+    var temp = (response.main.temp - 273.15) * 1.80 + 32;
+    $(".temp").text("Temperature:" + temp.toFixed(2) + "F");
+
+    // AJAX for UV Index:
+    $.ajax({
+      url: "http://api.openweathermap.org/data/2.5/uvi?" + encodeURIComponent(city) + apiKey,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
+      $(".uvIndex").text("UV Index:" + response)
+    })
+
+    // AJAX for 5 Day Forecast:
+    $.ajax({
+      url: "http://api.openweathermap.org/data/2.5/forecast?q=" + encodeURIComponent(city) + ",Burundi&appid=" + apiKey,
+      method: "GET"
+    })
+      .then(function (response) {
+        console.log(response);
+        $(".forecast").html("<h3>" + response.list.main.temp + " Weather Details</h3>");
+
+      })
+    // updateWeather(response.name, response.main.temp, response.main.humidity, response.wind.speed, "");
   });
 });
 
@@ -24,38 +51,45 @@ function setDate() {
   console.log(setDate);
 }
 
+setDate();
+
+
 // Display weather categories:
-function displayWeather() {
-  var weatherDiv = $("#results-card");
+// function displayWeather() {
+//   var weatherDiv = $("#results-card");
 
-  var currentDiv = $("<div " + "</div>");
-  var searchedCity = $("<h1>" + "City:" + "<span id='cityTag'></span>  </h1>");
-  var temperature = $("<p>" + "Temperature:" + "<span id='tempTag'></span> </p>");
-  var humidity = $("<p>" + "Humidity:" + "<span id='humidityTag'></span> </p>");
-  var wind = $("<p>" + "Wind:" + "<span id='windTag'></span> </p>");
-  var uvIndex = $("<p>" + "UV Index:" + "<span id='uvTag'></span> </p>");
+//   var currentDiv = $("<div> " + "</div>");
+  // var searchedCity = $("<h1>" + "City:" + "<span id='cityTag'></span>  </h1>");
+//   var temperature = $("<p>" + "Temperature:" + "<span id='tempTag'></span> </p>");
+//   var humidity = $("<p>" + "Humidity:" + "<span id='humidityTag'></span> </p>");
+//   var wind = $("<p>" + "Wind:" + "<span id='windTag'></span> </p>");
+//   var uvIndex = $("<p>" + "UV Index:" + "<span id='uvTag'></span> </p>");
 
 
-  currentDiv.append(searchedCity);
-  currentDiv.append(temperature);
-  currentDiv.append(humidity);
-  currentDiv.append(wind);
-  currentDiv.append(uvIndex);
-  weatherDiv.append(currentDiv);
-  console.log(displayWeather);
+//   currentDiv.append(searchedCity);
+//   currentDiv.append(temperature);
+//   currentDiv.append(humidity);
+//   currentDiv.append(wind);
+//   currentDiv.append(uvIndex);
+//   weatherDiv.append(currentDiv);
+//   console.log(displayWeather);
 
-};
+// };
 
 // Display weather results:
-function updateWeather(city, temperature, humidity, windSpeed, uvIndex) {
-  $("#cityTag").text(city);
-  $("#tempTag").text(temperature - 273.15) * 1.80 + 32;
-  $("#humidityTag").text(humidity);
-  $("#windTag").text(windSpeed);
+// function updateWeather(city, temperature, humidity, windSpeed, uvIndex) {
+//   $("#cityTag").text(city);
+//   $("#tempTag").text(temperature - 273.15) * 1.80 + 32;
+//   $("#humidityTag").text(humidity);
+//   $("#windTag").text(windSpeed);
 
-}
+//   $("#uvTag").text(uvIndex);
+// }
 
-displayWeather();
+
+
+
+// displayWeather();
 
 
 
